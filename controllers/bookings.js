5966be7b41287df180cb0bb1b9ab402a7e6996a8
kenmaready <ken@maready.com>
2021-04-24 13:34:56 -0400

    Fix ref error in bookings.js added console.logs for testing

--- controllers/bookings.js
+++ controllers/bookings.js
@@ -69,9 +69,11 @@ exports.getCheckout = catchWrapper(async (req, res) => {
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email }))._id;
-  const price = session.display_items[0].amount / 100;
+  const price = session.line_items[0].amount / 100;
 
+  console.log('{tour, user, price}:', { tour, user, price });
   await Booking.create({ tour, user, price });
+  console.log('new booking created...');
 };
 
 exports.webhookCheckout = (req, res, next) => {
